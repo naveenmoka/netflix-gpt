b943219e67a2e00c46ab829d3e9ebd1d54e37a9c
naveenmoka <naveenmoka2004@gmail.com>
2025-10-27 19:08:51 +0530

    Setting up TMBD API  & Get data from TMBD API

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,3 +2,11 @@ export const LOGO =
   "https://help.nflxext.com/helpcenter/OneTrust/oneTrust_production_2025-08-26/consent/87b6a5c0-0104-4e96-a291-092c11350111/0198e689-25fa-7d64-bb49-0f7e75f898d2/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png";
 export const USER_AVATAR =
   "https://occ-0-6247-2164.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABdpkabKqQAxyWzo6QW_ZnPz1IZLqlmNfK-t4L1VIeV1DY00JhLo_LMVFp936keDxj-V5UELAVJrU--iUUY2MaDxQSSO-0qw.png?r=e6e%22;";
+export const API_OPTIONS = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0OTY2YTU1ZGM2NGFmZjM2OWEzMTRjNTVkNTBlZjQyMyIsIm5iZiI6MTc2MTU3MDMwMy44MzAwMDAyLCJzdWIiOiI2OGZmNmRmZjdlNjM1Nzc1MDYzYmZlNTgiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.-2bhrG3kN8ldJEuk83p4RYMlKDSHSHbHTmJIXudoaPg",
+  },
+};
