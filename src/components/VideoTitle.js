bde4d2bf516d42bb0bb09ed676503624b98c67fb
naveenmoka <naveenmoka2004@gmail.com>
2025-10-27 23:50:30 +0530

    Building Video Background-2

--- src/components/VideoTitle.js
+++ src/components/VideoTitle.js
@@ -2,15 +2,15 @@ import React from "react";
 
 const VideoTitle = ({ title, overview }) => {
   return (
-    <div className="pt-36 px-12">
+    <div className="w-screen aspect-video px-24 pt-[20%] absolute text-white bg-gradient-to-r from-black">
       <h1 className="text-6xl font-bold">{title}</h1>
       <p className="py-6 text-lg w-1/4">{overview}</p>
       <div>
-        <button className="bg-gray-500 text-white p-4 px-12 text-xl bg-opacity-50 rounded-lg">
-          ▶️ Play
+        <button className="bg-white text-black p-4 px-12 text-xl hover:bg-opacity-50 rounded-lg">
+          ▶ Play
         </button>
-        <button className="bg-gray-500 mx-2 text-white p-4 px-12 text-xl bg-opacity-50 rounded-lg">
-          ℹ️ More Info
+        <button className="bg-gray-500 mx-2 text-white p-4 px-12 text-xl  hover:bg-opacity-50 rounded-lg">
+          More Info
         </button>
       </div>
     </div>
